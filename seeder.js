14a714f4b709a697a77de01d2e3518ec5714e156
alinili414e <alinili414e@gmail.com>
2022-05-23 23:18:06 -0400

    added seeder

--- seeder.js
+++ seeder.js
@@ -6,16 +6,21 @@ const env = require("dotenv");
 env.config({ path: "./config/config.env" });
 
 const Bootcamp = require("./models/Bootcamp");
-
+const Course = require("./models/Course");
 mongoose.connect(process.env.MONGO_URI);
 
 const bootcamps = JSON.parse(
   fs.readFileSync(`${__dirname}/_data/bootcamps.json`, "utf-8")
 );
 
+const courses = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/courses.json`, "utf-8")
+);
+
 const importData = async () => {
   try {
     await Bootcamp.create(bootcamps);
+    await Course.create(courses);
     console.log("Data Imported".green);
   } catch (error) {
     console.error(error);
@@ -25,6 +30,7 @@ const importData = async () => {
 const deleteData = async () => {
   try {
     await Bootcamp.deleteMany();
+    await Course.deleteMany();
     console.log("Data Destroyed".red);
   } catch (error) {
     console.error(error);
